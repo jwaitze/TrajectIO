8364996b2c1b8e5e27eb053f0a6dfb49a4587713
Jake Waitze <jake@waitze.net>
2016-07-11 00:04:38 -0400

    Fixed some line endings, need to add comments

--- client_side_only.js
+++ client_side_only.js
@@ -9,27 +9,27 @@ function gameMain() {
     manageSizes();
     drawBackground();
     drawGrid();
-    drawCircles()
+    drawCircles();
 };
 
 function getDistance(ax, ay, bx, by) {
-    return Math.sqrt(Math.pow(bx - ax, 2) + Math.pow(by - ay, 2))
+    return Math.sqrt(Math.pow(bx - ax, 2) + Math.pow(by - ay, 2));
 }
 
 function getDirection(sourceX, sourceY, targetX, targetY) {
-    return (180 * Math.atan2(targetY - sourceY, targetX - sourceX) / Math.PI + 360) % 360
+    return (180 * Math.atan2(targetY - sourceY, targetX - sourceX) / Math.PI + 360) % 360;
 }
 
 function getNewDirection(sumX, sumY) {
-    return (180 * Math.atan2(sumY, sumX) / Math.PI + 360) % 360
+    return (180 * Math.atan2(sumY, sumX) / Math.PI + 360) % 360;
 }
 
 function getVectorX(magnitude, direction) {
-    return magnitude * Math.cos(direction * Math.PI / 180)
+    return magnitude * Math.cos(direction * Math.PI / 180);
 }
 
 function getVectorY(magnitude, direction) {
-    return magnitude * Math.sin(direction * Math.PI / 180)
+    return magnitude * Math.sin(direction * Math.PI / 180);
 }
 
 function getNewVelocity(sumX, sumY) {
